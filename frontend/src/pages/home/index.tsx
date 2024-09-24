cb9cae4e4431e4514394e10282bbc6dd51eb1e9c
Ankit Yadav <ankityadav3152@gmail.com>
2024-09-24 18:35:08 +0530

    update canvas
    users can select colors from swatch and reset the canvas

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,37 @@
 import { useEffect, useRef, useState } from "react";
+import { SWATCHES } from "@/constants";
+import { ColorSwatch, Group, rgba } from "@mantine/core";
+import { Button } from "@/components/ui/button";
+import axios, { Axios } from "axios";
+
+interface GeneratedResult {
+  expression: string;
+  answer: string;
+}
+
+interface Response {
+  expr: string;
+  result: string;
+  assign: boolean;
+}
 
 export const Home = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [color, setColor] = useState("rgb(255, 255, 255)");
+  const [reset, setReset] = useState(false);
+  const [result, setResult] = useState<GeneratedResult>();
+  const [dictOfVars, setDictOfVars] = useState({});
 
+  // Reset state
+  useEffect(() => {
+    if (reset) {
+      resetCanvas();
+      setReset(false);
+    }
+  }, [reset]);
+
+  // Initialize the canvas
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
@@ -16,6 +44,35 @@ export const Home = () => {
       }
     }
   }, []);
+
+  // Function for sending the canvas image to backend
+  const sendData = async () => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const response = await axios({
+        url: `${import.meta.env.VITE_API_URL}/calculate`,
+        data: {
+          image: canvas.toDataURL("image/png"),
+          dict_of_vars: dictOfVars,
+        },
+      });
+
+      const resp = await response.data;
+      console.log("Response: ", resp);
+    }
+  };
+
+  // Function to reset the canvas
+  const resetCanvas = () => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    }
+  };
 
   // Function to start drawing on the canvas
   const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
@@ -46,7 +103,7 @@ export const Home = () => {
     if (canvas) {
       const ctx = canvas.getContext("2d");
       if (ctx) {
-        ctx.strokeStyle = "white";
+        ctx.strokeStyle = color;
         ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
         ctx.stroke();
       }
@@ -55,6 +112,33 @@ export const Home = () => {
 
   return (
     <>
+      <div className="grid grid-cols-3 gap-2">
+        <Button
+          onClick={() => setReset(true)}
+          className="bg-black text-white z-10"
+          variant="default"
+          color="black"
+        >
+          Reset Canvas
+        </Button>
+        <Group className="flex items-center justify-center z-10">
+          {SWATCHES.map((swatchColor: string) => (
+            <ColorSwatch
+              key={swatchColor}
+              color={swatchColor}
+              onClick={() => setColor(swatchColor)}
+            />
+          ))}
+        </Group>
+        <Button
+          onClick={sendData}
+          className="bg-black text-white z-10"
+          variant="default"
+          color="black"
+        >
+          Calculate
+        </Button>
+      </div>
       <canvas
         ref={canvasRef}
         id="canvas"
