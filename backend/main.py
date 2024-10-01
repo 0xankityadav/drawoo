cc63ecacb458d6de677ba225efd75a2ede15f557
Ankit Yadav <ankityadav3152@gmail.com>
2024-10-01 23:02:52 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,12 +11,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = ["https://drawoo-backend.vercel.app"];
-
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
